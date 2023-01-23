9bbf48b945e4b639d6a2108d09397ea256264894
KarakozovaRegina <105420067+KarakozovaRegina@users.noreply.github.com>
2023-01-23 23:49:19 +0300

    Add files via upload

---
+++ lb-2/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lb_2
+{
+    public partial class Bus
+    {
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+            Bus b = obj as Bus; // возвращает null если объект нельзя привести к типу bus
+            if (b as Bus == null)
+                return false;
+
+            return b.id == this.id;
+
+        }
+        public override string ToString()
+        {
+            return ($"\nSurname: {this.surname}\nMarka: {this.marka}\nId: {this.id}");
+        }
+
+        public override int GetHashCode()
+        {
+            return (int)startYear;
+        }
+    }
+}
---
+++ lb-2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lb_2
+{
+    public class Program
+    {
+       public static void Main()
+        {
+            Bus[] buss = new Bus[3];
+            buss[0] = new Bus("Karakozova", "KRV", "4689", "M1", "BMV", 2006, "10000", 1245);
+            buss[1] = new Bus("Ivanov", "IHY", "1119", "M2", "Porshe", 2007, "2000", 6854);
+            buss[2] = new Bus("Stevanov", "WWW ", "8079", "M3", "BMV", 2015, "73000", 5775);
+           
+
+            foreach (Bus b in buss)
+            {
+                    b.Print();
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("With BMV");
+            foreach (Bus k in buss)
+            {
+                if (k.marka == "BMV")
+                {
+                    k.Printt();
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine(">2010");
+            foreach (var i in buss)
+            {
+                if (i.startYear> 2010)
+                {
+                    i.Printt();
+                }
+            }
+
+            Bus.Kolichestvo();
+            Console.WriteLine("Equals: " + buss[0].Equals(buss[1]));
+            Console.WriteLine("ToString: " + buss[0].ToString() + "\n");
+            Console.WriteLine("GetHashCode: " + buss[1].GetHashCode());
+
+            var user = new { Surame = "Tomuif", theFirstLetters = "GGG" };
+            Console.WriteLine("Surame: " + user.Surame + " TheFirstLetters: " + user.theFirstLetters);
+            
+            Console.ReadLine();
+        }
+    }
+}
