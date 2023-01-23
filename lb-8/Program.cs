c87108483cd66abe2a383b05e19f5d01e5abeab0
KarakozovaRegina <105420067+KarakozovaRegina@users.noreply.github.com>
2023-01-23 23:51:56 +0300

    Add files via upload

---
+++ lb-8/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//1. Используя делегаты (множественные) и события промоделируйте ситуации. При реализации методов везде где возможно использовать лямбда-выражения.
+//Создать класс Игра с событиями Атака и Лечить.
+namespace Lab_OOP_8
+{
+    public class Game
+    {
+        public string name;
+        public int health;
+        public Game()
+        {
+            name = "NoName";
+            health = 100;
+        }
+        public Game(string name, int health)
+        {
+            this.name = name;
+            this.health = health;
+        }
+        public event EventHandler<ActArgs> Attack;      //события на основе стандартного делегата
+        public event EventHandler<ActArgs> Treatment;
+        public void ToAtt()    //метод, инициирующий событие
+        {
+            Console.WriteLine(this.name + ", you have been attacked.");
+            Attack?.Invoke(this, new ActArgs(this.health));//вызов события, если не null
+        }
+        public void ToTr()
+        {
+            Console.WriteLine(this.name + ", you have been treated.");
+            Treatment?.Invoke(this, new ActArgs(this.health));
+        }
+    }
+
+
+
+
+}
---
+++ lb-8/StringHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_OOP_8
+{
+    public static class StringHandler
+    {
+        //2.Создайте пять методов пользовательской обработки строки (например, удаление знаков препинания, добавление символов, замена на заглавные,
+        //удаление лишних пробелов и т.п.). Используя стандартные типы делегатов (Action, Func) организуйте алгоритм последовательной обработки строки
+        //написанными вами методами.
+        public static string RemoveS(string str, Func<string, string> test1) { return test1(str); }      //удаление знаков
+        public static void AddToString(string str, Action<string> test2) => test2(str);                 //добавление строки
+        public static string RemoveSpase(string str, Func<string, string> test3) { return test3(str); }  //удаление пробелов
+        public static string Upper(string str, Func<string, string> test4) { return test4(str); }       //в верхний регистр
+        public static string Lower(string str, Func<string, string> test5) { return test5(str); }       //в нижний регистр
+    }
+}
+
+
+
+
+
+
+
+
+
+
+/*Делегат Action представляет некоторое действие, которое ничего не возвращает, то есть в качестве возвращаемого типа имеет тип void:
+Как правило, этот делегат передается в качестве параметра метода и предусматривает вызов определенных действий в ответ на произошедшие действия.*/
+
+/*Func Он возвращает результат действия и может принимать параметры.*/
+
+//изменение и возвращение 
