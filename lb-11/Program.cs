3142f067d55fa6588b413d975121bca08c03876a
KarakozovaRegina <105420067+KarakozovaRegina@users.noreply.github.com>
2023-01-23 23:54:52 +0300

    Add files via upload

---
+++ lb-11/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+
+
+namespace OOP_Lab_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            Car car = new Car("Porsche", "911");
+            Console.WriteLine(++i + ". " + Reflector.GetAssemblyVesion());
+
+            Console.WriteLine(++i + ". " + (Reflector.IncludeConstructor(car) ? "Car include public constructor" :
+                "Car doesn't include public constructor"));
+
+            Console.Write($"{++i}. ");
+            Reflector.GetPublicMethods(car);
+            Console.WriteLine();
+
+            Console.Write($"{++i}. ");
+            Reflector.GetPropetry(car);
+            Reflector.GetFields(car);
+            Console.WriteLine();
+
+            Console.Write($"{++i}. ");
+            Reflector.GetInterfaces(car);
+            Console.WriteLine();
+
+            Console.Write($"{++i}. ");
+            Reflector.GetMethodsByParam(car, "Int32 i");
+            Console.WriteLine();
+
+            Console.Write($"{++i}. ");
+            Reflector.InvokeFromFile();
+
+            object ob = Reflector.Create("Toyota", "Camry");
+            Console.Write($"{++i}. {(ob as Car).Brand} {(ob as Car).Model}");
+
+            Console.ReadLine();
+        }
+    }
+}
