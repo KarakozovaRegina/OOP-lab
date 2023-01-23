08779ddfb5f437db8bf5cc7e440e5e4d49dbe96d
KarakozovaRegina <105420067+KarakozovaRegina@users.noreply.github.com>
2023-01-23 23:49:40 +0300

    Add files via upload

---
+++ Classes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lb_7
+{
+    class Inventory
+    {
+    }
+
+    class Ball : Inventory
+    {
+        public override string ToString() => "Ball'";
+    }
+
+    class Mate : Inventory
+    {
+        public override string ToString() => "Mate";
+    }
+}
