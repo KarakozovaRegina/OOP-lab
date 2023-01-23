19b4a80a8d3172e1696159803a08941cb51d1934
KarakozovaRegina <105420067+KarakozovaRegina@users.noreply.github.com>
2023-01-23 23:54:12 +0300

    Add files via upload

---
+++ Classes.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Lab_12
+{
+    class Car : ICloneable
+    {
+        private string _brand;
+        private string _model;
+
+        private Car()
+        {
+
+        }
+
+        public Car(string brand, string model)
+        {
+            _brand = brand;
+            _model = model;
+        }
+
+        public string Brand
+        {
+            get => _brand;
+            set => _brand = value;
+        }
+        public string Model => _model;
+
+        public void GetId(int i) => Console.WriteLine(Convert.ToInt32(_model[0]) * i);
+        public object Clone() => MemberwiseClone();
+    }
+}
